4c2ee2ed189238ab38f68b80875cc25a40ded7f1
Tousif Tamboli <142837294+TousifTamboli@users.noreply.github.com>
2024-10-24 19:25:16 +0530

    exp 6 knapsack using dp simple

--- exp6.cpp
+++ exp6.cpp
@@ -1,33 +1,59 @@
+// Online C++ compiler to run C++ program online
 #include <iostream>
 #include <vector>
 #include <algorithm>
+
 using namespace std;
 
-// Function to solve the 0/1 Knapsack problem using Dynamic Programming
-int knapsack(int W, int weights[], int values[], int n) {
-    int dp[n + 1][W + 1];
-    
-    // Initialize the dp array
-    for (int i = 0; i <= n; i++) {
-        for (int w = 0; w <= W; w++) {
-            if (i == 0 || w == 0) {
-                dp[i][w] = 0; // Base case
-            } else if (weights[i - 1] <= w) {
-                dp[i][w] = max(values[i - 1] + dp[i - 1][w - weights[i - 1]], dp[i - 1][w]);
-            } else {
-                dp[i][w] = dp[i - 1][w];
+int solveMem(vector<int>& weight, vector<int>& value, int index, int capacity, vector<vector<int>>& dp){
+        
+        //base case
+        //if only one item is present compare its weight with knapsack
+        if(index == 0){
+            if(weight[0] <= capacity){
+                return value[0];
             }
+            else
+                return 0;
         }
+        
+        if(dp[index][capacity] != -1)
+            return dp[index][capacity];
+        
+        //ya include karo ya exclude karo
+        
+        //include
+        int include = 0;
+        if(weight[index] <= capacity)
+            include = value[index] + solveMem(weight, value, index - 1, capacity - weight[index], dp);
+        
+        //exclude        
+        int exclude = 0 + solveMem(weight, value, index - 1, capacity, dp);
+        
+        //index aur capacity ki value change hori hai
+        dp[index][capacity] = max(exclude, include);
+        
+        return dp[index][capacity];
+        
     }
-    return dp[n][W];
-}
 
 int main() {
-    int values[] = {60, 100, 120};
-    int weights[] = {10, 20, 30};
-    int W = 50; // Maximum weight capacity of the knapsack
-    int n = sizeof(values) / sizeof(values[0]);
+    
+    vector<int> weight = {10, 20, 30}; 
+    vector<int> value = {60, 100, 120}; 
+    
+    
+    int n = weight.size(); 
+    int capacity = 50; 
+    
+    // 2D dp array banao
+    vector<vector<int>> dp(n, vector<int>(capacity+1, -1));
+    
+    
+    int maxValue = solveMem(weight, value, n - 1, capacity, dp);
+    
+    //final output
+    cout << "Maximum value in Knapsack = " << maxValue << endl;
 
-    cout << "Maximum value in Knapsack = " << knapsack(W, weights, values, n) << endl;
     return 0;
 }
